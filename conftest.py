3103e008bb6de3e1abb3130ca42222f741432b91
aigulmustafina <aimustafina2016@gmail.com>
2023-05-09 04:36:56 +0600

    feat: add allure report

--- conftest.py
+++ conftest.py
@@ -1,4 +1,7 @@
 import os
+import shutil
+
+import allure
 import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
@@ -38,3 +41,31 @@ def open_and_load_main_page(driver, wait):
 def wait(driver):
     wait = WebDriverWait(driver, 25)
     yield wait
+
+def pytest_runtest_makereport(item, call):
+    if call.when == 'call':
+        # test_name = item.name
+        if call.excinfo is not None:
+            # status = 'failed'
+            # logger.error(f'{status} - {test_name}. Reason: {str(call.excinfo)}')
+            try:
+                driver = item.funcargs['driver']
+                driver.save_screenshot('allure-results/screenshot.png')
+                allure.attach.file('allure-results/screenshot.png', name='Screenshot',
+                                   attachment_type=allure.attachment_type.PNG)
+                allure.attach(driver.page_source, name="HTML source", attachment_type=allure.attachment_type.HTML)
+            except Exception as e:
+                print(f"Failed to take screenshot: {e}")
+
+def pytest_sessionstart(session):
+    allure_report_dir = "allure-results"
+    if os.path.exists(allure_report_dir):
+        for file_name in os.listdir(allure_report_dir):
+            file_path = os.path.join(allure_report_dir, file_name)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print(f"Failed to delete {file_path}. Reason: {e}")
